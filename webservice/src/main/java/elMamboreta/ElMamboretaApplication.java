f34363f51ad2e66eeda675977f3f382816e7d657
FrancoSaracho64 <fmsaracho64@gmail.com>
2025-07-30 19:31:49 -0300

    Base del proyecto

---
+++ webservice/src/main/java/elMamboreta/ElMamboretaApplication.java
@@ -0,0 +1,13 @@
+package elMamboreta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElMamboretaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElMamboretaApplication.class, args);
+	}
+
+}
---
+++ webservice/src/test/java/elMamboreta/ElMamboretaApplicationTests.java
@@ -0,0 +1,13 @@
+package elMamboreta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElMamboretaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
